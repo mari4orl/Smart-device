298ca4059fb4b8bdc5d72cd8ed61c893707e8aaa
Richie <mari4orl@mail.ru>
2020-11-06 15:39:04 +0300

    Добавляет сборку

---
+++ source/js/navigation.js
@@ -0,0 +1,15 @@
+'use strict'
+var navigation = document.querySelector('.navigation');
+var navToggle = document.querySelector('.navigation__toggle');
+
+navigation.classList.remove('navigation--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navigation.classList.contains('navigation--closed')) {
+    navigation.classList.remove('navigation--closed');
+    navigation.classList.add('navigation--opened');
+  } else {
+    navigation.classList.add('navigation--closed');
+    navigation.classList.remove('navigation--opened');
+  }
+});
